5699fde9e3c0c46b5dff9196f3ca5aaed2a14386
Rafael <rafaelfagundes762@gmail.com>
2023-09-14 19:39:54 -0300

    adicionando segundo carrosel de produtos iniciais e trocando wallpaper de fundo.

--- index.php
+++ index.php
@@ -101,7 +101,7 @@
     <div class="container">
 
 
-        <div class="card border-5" style="text-align: center;" >
+        <div class="card border-5" style="text-align: center;">
             <div class="card-body">
                 <i class="mdi mdi-account-lock-open"></i> Segurança nas suas compras |
                 <i class="mdi mdi-account-cash"> </i> Cabe no seu Bolso |
@@ -138,11 +138,13 @@
         <img src="img/marcas/deathNote.png" alt="..." width="150px" class="mx-3">
     </marquee>
 
-    <br><br><br><br><br><br><br>
+    <br><br><br><br><br>
 
+    <div class="container">
+        <p class="fs-5" style="color: black;"> Produtos Com Desconto</p>
 
-
-    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    </div>
+    <div id="prodDestaque" class="carousel slide" data-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
                 <div class="container">
@@ -259,11 +261,144 @@
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="sr-only">Previous</span>
         </a>
+        <a class="carousel-control-next" href="#prodDestaque" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+
+
+
+    <!-- segundo carrosel -->
+
+    <br><br><br>
+    <div class="container">
+        <p class="fs-5" style="color: black;"> Produtos Destaque</p>
+
+    </div>
+    <div id="prodDestaqueDois" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <div class="container">
+                    <div class="card-group">
+
+                        <!-- Primeiro grupo de cards -->
+
+                        <div class="card mb-3 ">
+                            <img src="img/produtos/esqueletao.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Eskeleto</h5>
+                                <p class="card-text" style="color: black; font-size: 18pt ;">R$600,00</p>
+                                <p class="card-text" style="color: LimeGreen;"><b>Desconto 50%</b></p>
+                                <p class="card-text" style="color: LimeGreen;"><b>Frete Gratis</b></p>
+                            </div>
+                        </div>
+                        <div class="card mb-3 ">
+                            <img src="img/produtos/esqueletao.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Eskeleto</h5>
+                                <p class="card-text" style="color: black; font-size: 18pt ;">R$600,00</p>
+                                <p class="card-text" style="color: LimeGreen;"><b>Desconto 50%</b></p>
+                                <p class="card-text" style="color: LimeGreen;"><b>Frete Gratis</b></p>
+                            </div>
+                        </div>
+                        <div class="card mb-3 ">
+                            <img src="img/produtos/esqueletao.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Eskeleto</h5>
+                                <p class="card-text" style="color: black; font-size: 18pt ;">R$600,00</p>
+                                <p class="card-text" style="color: LimeGreen;"><b>Desconto 50%</b></p>
+                                <p class="card-text" style="color: LimeGreen;"><b>Frete Gratis</b></p>
+                            </div>
+                        </div>
+                        <div class="card mb-3 ">
+                            <img src="img/produtos/esqueletao.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Eskeleto</h5>
+                                <p class="card-text" style="color: black; font-size: 18pt ;">R$600,00</p>
+                                <p class="card-text" style="color: LimeGreen;"><b>Desconto 50%</b></p>
+                                <p class="card-text" style="color: LimeGreen;"><b>Frete Gratis</b></p>
+                            </div>
+                        </div>
+                        <div class="card mb-3 ">
+                            <img src="img/produtos/esqueletao.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Eskeleto</h5>
+                                <p class="card-text" style="color: black; font-size: 18pt ;">R$600,00</p>
+                                <p class="card-text" style="color: LimeGreen;"><b>Desconto 50%</b></p>
+                                <p class="card-text" style="color: LimeGreen;"><b>Frete Gratis</b></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="carousel-item">
+                <div class="container">
+                    <div class="card-group">
+                        <div class="card mb-3">
+                            <img src="img/produtos/deadpool.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">deadpool</h5>
+                                <p class="card-text" style="color: black; font-size: 18pt ;">R$1600,00</p>
+                                <p class="card-text" style="color: LimeGreen;"><b>Desconto 30%</b></p>
+                                <p class="card-text" style="color: LimeGreen;"><b>Frete Gratis</b></p>
+                            </div>
+                        </div>
+                        <div class="card mb-3">
+                            <img src="img/produtos/deadpool.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">deadpool</h5>
+                                <p class="card-text" style="color: black; font-size: 18pt ;">R$1600,00</p>
+                                <p class="card-text" style="color: LimeGreen;"><b>Desconto 30%</b></p>
+                                <p class="card-text" style="color: LimeGreen;"><b>Frete Gratis</b></p>
+                            </div>
+                        </div>
+                        <div class="card mb-3">
+                            <img src="img/produtos/deadpool.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">deadpool</h5>
+                                <p class="card-text" style="color: black; font-size: 18pt ;">R$1600,00</p>
+                                <p class="card-text" style="color: LimeGreen;"><b>Desconto 30%</b></p>
+                                <p class="card-text" style="color: LimeGreen;"><b>Frete Gratis</b></p>
+                            </div>
+                        </div>
+                        <div class="card mb-3">
+                            <img src="img/produtos/deadpool.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">deadpool</h5>
+                                <p class="card-text" style="color: black; font-size: 18pt ;">R$1600,00</p>
+                                <p class="card-text" style="color: LimeGreen;"><b>Desconto 30%</b></p>
+                                <p class="card-text" style="color: LimeGreen;"><b>Frete Gratis</b></p>
+                            </div>
+                        </div>
+                        <div class="card mb-3">
+                            <img src="img/produtos/deadpool.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">deadpool</h5>
+                                <p class="card-text" style="color: black; font-size: 18pt ;">R$1600,00</p>
+                                <p class="card-text" style="color: LimeGreen;"><b>Desconto 30%</b></p>
+                                <p class="card-text" style="color: LimeGreen;"><b>Frete Gratis</b></p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+
+        <a class="carousel-control-prev" href="#prodDestaqueDois" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
         <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="sr-only">Next</span>
         </a>
     </div>
+
 
 
 
