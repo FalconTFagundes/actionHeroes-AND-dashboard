3d8c6887b9601d52a9053c085c8dc51038b3d3a4
Rafael <rafaelfagundes762@gmail.com>
2023-09-14 20:18:28 -0300

    Adicionando card com botão ancorado na página de ofertas

--- ofertasDoDia.php
+++ ofertasDoDia.php
@@ -1,7 +1,22 @@
 <?php include_once 'head.php'; ?>
 <?php include_once 'navbar.php'; ?>
+<br><br>
 
 <div class="container">
+  <div class="card text-center">
+    <div class="card-header">
+      Ofertas do Dia
+    </div>
+    <div class="card-body">
+      <h5 class="card-title">Conheça as nossas especiais ofertas</h5>
+      <p class="card-text">Todos os dias uma oferta diferente é adicionada.</p>
+      <a href="#prodMeio" class="btn btn-primary">Veja mais</a>
+    </div>
+    <div class="card-footer text-muted">
+      Action Figures
+    </div>
+  </div>
+  <br><br>
 
 
   <div class="row row-cols-1 row-cols-md-3 g-4">
@@ -32,7 +47,7 @@
         </div>
       </div>
     </div>
-    <div class="col">
+    <div class="col" id="prodMeio">
       <div class="card">
         <img src="..." class="card-img-top" alt="...">
         <div class="card-body">
