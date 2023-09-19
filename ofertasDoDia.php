001c5c21deb137a7b3ddab94e53c2d45b759770d
luishenriquesenai <0000939921@senaimgaluno.com.br>
2023-09-18 21:24:04 -0300

    Adicionando imagens dos produtos, e continuando a ajustar proporções

--- ofertasDoDia.php
+++ ofertasDoDia.php
@@ -22,7 +22,10 @@
   <div class="row row-cols-1 row-cols-md-3 g-4">
     <div class="col">
       <div class="card">
-        <img src="..." class="card-img-top" alt="...">
+        <div class="image-container" style="height: 400px;">
+          <img src="./img/produtos/batman1.jpg" class="card-img-top " alt="...">
+        </div>
+
         <div class="card-body">
           <h5 class="card-title">Card title</h5>
           <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -31,9 +34,15 @@
     </div>
     <div class="col">
       <div class="card">
+<<<<<<< HEAD
+      <div class="image-container" style="height: 400px;">
+        <img src="./img/produtos/bane1.jpg" class="card-img-top" alt="">
+      </div>
+=======
         <div class="image-container" style="height: 300px;">
           <img src="img/produtos/esqueletao.png" class="card-img-top" alt="...">
         </div>
+>>>>>>> 981d4748b4c4e7e9ad4f5063880d7fcabfa86061
         <div class="card-body">
           <h5 class="card-title">Card title</h5>
           <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -41,11 +50,19 @@
       </div>
     </div>
     <div class="col">
+<<<<<<< HEAD
+      <div class="car">
+      <div class="image-container" style="height: 400px;">
+        <img src="./img/produtos/freeza1.jpg" class="card-img-top " alt="...">
+      
+=======
       <div class="card">
         <div class="image-container" style="height: 300px;">
           <img src="img/produtos/deadpool.png" class="card-img-top" alt="...">
         </div>
+>>>>>>> 981d4748b4c4e7e9ad4f5063880d7fcabfa86061
         <div class="card-body">
+        </div>
           <h5 class="card-title">Card title</h5>
           <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
         </div>
