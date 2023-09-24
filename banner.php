28947aa07148e7a5b1e8732914b3237ec1f99a03
Rafael <rafaelfagundes762@gmail.com>
2023-09-24 15:49:02 -0300

    adicionando arquivo .sql, puxando informações do banco para o banner e adicionando funções PHP

--- banner.php
+++ banner.php
@@ -1,34 +1,31 @@
- <!-- Carousel Start -->
- <div class="container-fluid p-0 wow fadeIn" data-wow-delay="0.1s">
-        <div id="header-carousel" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img class="w-100 custom-height-banner" src="./img/banner/batman.jpg" alt="Image">
-                </div>
+<div class="container-fluid p-0 wow fadeIn" data-wow-delay="0.1s">
+    <div id="banner" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <?php
+            $retornoBanner = listarTodosRegistros('banner','idbanner, img, cadastro, alteracao, ativo','A');
+            $primeiraImagem = true;
 
+            foreach ($retornoBanner as $itemBanner) {
+                $bannerImg = $itemBanner->img;
+            ?>
 
-                <div class="carousel-item">
-                    <img class="w-100 custom-height-banner" src="img/banner/superman.jpg" alt="Image">
-                </div>
-
-
-                <div class="carousel-item">
-                    <img class="w-100 custom-height-banner" src="img/banner/deadpool.jpg" alt="Image">
-                </div>
+            <div class="carousel-item <?php echo $primeiraImagem ? 'active' : ''; ?>">
+                <img class="w-100 custom-height-banner" src="./img/banner/<?php echo $bannerImg; ?>" alt="Image">
             </div>
-
-
-
-            <button class="carousel-control-prev" type="button" data-bs-target="#header-carousel" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#header-carousel" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
+            
+            <?php
+                $primeiraImagem = false;
+            }
+            ?>
         </div>
-    </div>
 
-    
-  
+        <button class="carousel-control-prev" type="button" data-bs-target="#banner" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#banner" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+</div>
