837ec98dc98ffcaa279197ed849b81418c38042f
Rafael <rafaelfagundes762@gmail.com>
2023-09-30 12:39:16 -0300

    adicionando função de recorte e upload de imagem, adicionando modal de cadastro - banner

--- dashboard/empresa/bannerDashboard.php
+++ dashboard/empresa/bannerDashboard.php
@@ -7,6 +7,12 @@ include_once 'config/constantes.php';
 
 ?>
 
+<!-- BTN CADASTRAR BANNER -->
+<button type="submit" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalCadastrarBanner">
+  Cadastrar
+</button>
+
+
 <table class="table table-striped">
   <thead>
     <tr>
@@ -39,14 +45,46 @@ include_once 'config/constantes.php';
         <td><?php echo $imgBanner; ?></td>
 
         <td>
-          <button type="button" class="btn btn-primary">Veja</button>
           <button type="button" class="btn btn-danger">Excluir</button>
-          <button type="button" class="btn btn-success">Editar</button>
+          <button type="button" class="btn btn-success">Ativar</button>
         </td>
 
-      
+
 
       </tr>
     <?php    } ?>
   </tbody>
 </table>
+
+
+
+
+
+<!-- MODAL CADASTRAR BANNER -->
+<div class="modal fade" id="modalCadastrarBanner" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-lg">
+    <div class="modal-content">
+      <div class="modal-header bg-dark">
+        <h5 class="modal-title align-items-center" id="exampleModalLabel">Cadastrar Banner</h5>
+      </div>
+      <div class="modal-body">
+        <form>
+          <div class="mb-3">
+            <label for="arquivoBanner" class="form-label">Enviar Arquivo</label>
+            <input type="file" name="arquivoBanner" class="form-control" id="arquivoBanner" aria-describedby="arquivoBanner">
+          </div>
+          <div id="previewUploadBanner"> </div>
+      </div>
+
+      <div class="modal-footer">
+        <button type="submit" class="btn btn-info" id="bntUploadArquivoBanner"><i class="mdi mdi-content-save-move"></i> Salvar</button>
+        <button type="button" class="btn btn-warning" data-bs-dismiss="modal"><i class="mdi mdi-close"></i> Cancelar</button>
+        </form>
+        
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<script src="./js/painel.js"></script>
---
+++ dashboard/upload.php
@@ -0,0 +1,23 @@
+<?php 
+
+// Receber a imagem
+$imagem = filter_input(INPUT_POST, 'imagem', FILTER_DEFAULT);
+//var_dump($imagem);
+
+// Separa as informações da imagem base64 pelo ";"
+list($type, $imagem) = explode(';', $imagem);
+list(, $imagem) = explode(',', $imagem);
+
+// Desconverter a imagem base64
+$imagem = base64_decode($imagem);
+
+// Atribuir a extensão da imagem PNG
+$imagem_nome = time() . '.png';
+
+// Realizar o upload da imagem
+$uploadDir = '../img/banner';
+file_put_contents($uploadDir . $imagem_nome, $imagem);
+
+echo "Imagem enviada com sucesso!";
+
+?>
